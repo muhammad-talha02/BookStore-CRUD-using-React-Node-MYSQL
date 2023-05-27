aa93749db5e91d15338935c127920ac0f295b86e
muhammad-talha02 <iamtalha971@gmail.com>
2023-05-27 15:41:11 +0500

    Added Upload image functionality on book using multer

--- bookstore-ui/src/pages/Add.jsx
+++ bookstore-ui/src/pages/Add.jsx
@@ -7,20 +7,42 @@ function Add() {
     const [book, setBook] = useState({
         title: "",
         description: "",
-        cover: "",
         price: '',
-        author: ''
+        author: '',
     });
+    const [file, setFile] = useState({})
     const navigate = useNavigate()
+    console.log(book)
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setBook(prevState => ({ ...prevState, [name]: value }))
     }
+    const handleFileChange = (e) => {
+        console.log(e.target.files[0])
+        setFile(e.target.files[0])
+    }
+
+
+
     const handleClick = async (e) => {
         e.preventDefault();
+        const formData = new FormData();
+        formData.append("title", book.title);
+        formData.append("description",  book.description);
+        formData.append("file", file);
+        formData.append("price", book.price);
+        formData.append("author", book.author);
+        console.log(formData);
+
+        const config = {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        }
         try {
-            await axios.post("http://localhost:8000/books", book);
+           const res =  await axios.post("http://localhost:8000/books", formData);
+            console.log(res)
             navigate("/")
         } catch (error) {
             console.log(error)
@@ -45,14 +67,14 @@ function Add() {
                             <input type='number' className='form-control' value={book.price} name='price' onChange={handleChange} placeholder='Price' />
                         </div>
                         <div className="col-12 col-md-6 g-3">
-                            <input type='text' className='form-control' value={book.cover} name='cover' onChange={handleChange} placeholder='Cover' />
+                            <input type='file' className='form-control' name='file' onChange={handleFileChange} placeholder='Cover' />
                         </div>
                         <div className="col-12 col-md-12 g-3">
                             <textarea type='text' className='form-control' value={book.description} name='description' onChange={handleChange} placeholder='Description' />
                         </div>
 
                     </div>
-                <UploadFiles/>
+                    {/* <UploadFiles/> */}
                     <div className='d-flex justify-content-between'>
                         <button className='btn btn-secondary float-right my-3' onClick={() => navigate("/")}>Go Back</button>
                         <button className='btn btn-primary float-right my-3' onClick={handleClick}>Submit</button>
--- server/index.js
+++ server/index.js
@@ -1,95 +1,17 @@
-import express from "express";
-import mysql from "mysql";
-import cors from "cors"
+const express = require("express")
+const cors = require("cors")
+require("./db/conn")
+const router = require("./routes/router")
 const port = 8000;
 
 const app = express();
-app.use(cors())
-
-
 app.use(express.json())
-
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root@123",
-    database: "bookstore"
-})
+app.use(cors())
+app.use(router)
+app.use("/uploads", express.static("uploads"))
 
 
-app.get("/", (req, res) => {
-    res.send("Hi")
-})
 
-
-// Get All Books
-
-app.get("/books", (req, res) => {
-    const q = "SELECT * FROM books";
-    db.query(q, (err, data) => {
-        if (err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-// Add New Book
-
-app.post("/books", (req, res) => {
-    const q = "INSERT INTO books(title, description, cover, price, author) VALUES (?)"
-    const values = [
-        req.body.title,
-        req.body.description,
-        req.body.cover,
-        req.body.price,
-        req.body.author,
-    ];
-
-    db.query(q, [values], (err, data) => {
-        if (err) return res.json(err);
-        return res.json("Book added successfully")
-    })
-});
-
-
-// Get Specific Book
-
-app.get("/book/:id", (req, res) => {
-    const bookId = req.params.id;
-    const q = "SELECT * from books WHERE id = ?";
-    db.query(q, [bookId], (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data)
-    })
-});
-
-// UpdATE A BOOK
-
-app.put("/book/:id", (req, res) => {
-    const bookId = req.params.id;
-    const q = "UPDATE books SET title=? , description=?, cover=?, price=?, author=? WHERE id = ?"
-    const values = [
-        req.body.title,
-        req.body.description,
-        req.body.cover,
-        req.body.price,
-        req.body.author,
-    ];
-    db.query(q, [...values, bookId], (err, data) => {
-        if (err) return res.json(err);
-        return res.json("Book Updated successfully")
-    })
-})
-
-// Delete a Book
-
-app.delete("/book/:id", (req, res) => {
-    const bookId = req.params.id;
-    const q = "DELETE from books WHERE id = ?";
-    db.query(q, [bookId], (err, data) => {
-        if (err) return res.json(err);
-        return res.json("Book Deleted Successfully")
-    })
-})
 
 app.listen(port, () => {
     console.log("App is Running on", port)
